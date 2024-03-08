a8f1a2b872766758b1cf37180b1360d3a7d473ea
David Budzynski <david.budzynsky@gmail.com>
2024-03-08 08:21:35 +0000

    fix: folder must be all lowercase

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -34,7 +34,12 @@ if [ -z "$GIT_USER_EMAIL" ]; then
 fi
 
 # Get the name of the current folder
-current_folder=$(basename "$(pwd)")
+current_folder=$(basename "$(pwd)") 
+
+# Convert current_folder to lowercase if it contains uppercase characters
+if [[ "$current_folder" =~ [A-Z] ]]; then
+    current_folder=$(echo "$current_folder" | tr '[:upper:]' '[:lower:]')
+fi
 
 # Build the Docker image
 docker build \
